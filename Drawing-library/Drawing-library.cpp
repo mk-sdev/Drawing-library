119364ddc64c25d023b916af5666773c6888a8fa
agentnyrro <qwerty2name@gmail.com>
2024-06-03 21:01:56 +0200

    write definitions for textDisplay functions

--- Drawing-library/Drawing-library.cpp
+++ Drawing-library/Drawing-library.cpp
@@ -1,40 +1,40 @@
 ﻿#include <iostream>
 #include "graphicalDisplay.h"
-//#include "textDisplay.h"
+#include "textDisplay.h"
 #include "circle.h"
 #include "triangle.h"
 #include "parallelogram.h"
-//#include "complexShape.h"
-//#include "display.h"
-//#include "shape.h"
+#include "complexShape.h"
+#include "display.h"
+#include "shape.h"
 using namespace std;
 int main() {
 	Display* w1 = new GraphicalDisplay();
-	//Display* w2 = new TextDisplay();
+	Display* w2 = new TextDisplay();
 	Shape* o1 = new Triangle(w1, 3, 2, 2, -2);
 	Shape* o2 = new Parallelogram(w1, 3, 2, 2, -2);
 	Shape* o3 = new Circle(w1, 5);
-	//ComplexShape* o4 = new ComplexShape(w1);
-	//o4->add(o1);
-	//o4->add(o2);
-	//o4->add(o3);
-	//o4->draw();
+	ComplexShape* o4 = new ComplexShape(w1);
+	o4->add(o1);
+	o4->add(o2);
+	o4->add(o3);
+	o4->draw();
 	o1->draw();
 	o2->draw();
 	o3->draw();
-	//o1->changeDisplay(w2);
-	//o2->changeDisplay(w2);
-	//o3->changeDisplay(w2);
-	//o4->changeDisplay(w2);
-	//o4->draw();
-	//o1->draw();
-	//o2->draw();
-	//o3->draw();
+	o1->changeDisplay(w2);
+	o2->changeDisplay(w2);
+	o3->changeDisplay(w2);
+	o4->changeDisplay(w2);
+	o4->draw();
+	o1->draw();
+	o2->draw();
+	o3->draw();
 	delete o1;
 	delete o2;
 	delete o3;
-	//delete o4;
-	//delete w2;
-	//delete w1;
+	delete o4;
+	delete w2;
+	delete w1;
 	return 0;
 }
--- Drawing-library/textDisplay.h
+++ Drawing-library/textDisplay.h
@@ -1,7 +1,35 @@
 #pragma once
 #include "display.h"
+
+/**
+ * Klasa sluzaca do tekstowego wyswietlania obiektow
+ */
 class TextDisplay :
     public Display
 {
+public:
+    /**
+     * Funkcja wypisujaca tekst: Drawing a triangle from vectors (a1, a2), (b1, b2).
+     * @param a1 - wspolrzedna x pierwszego wierzcholka
+     * @param a2 - wspolrzedna y pierwszego wierzcholka
+     * @param b1 - wspolrzedna x drugiego wierzcholka
+     * @param b2 - wspolrzedna y drugiego wierzcholka
+     */
+    void drawTriangle(int a1, int a2, int b1, int b2) override;
+
+    /**
+     * Funkcja wypisujaca tekst: Drawing a circle with radius r.
+     * @param r - Promien okregu
+     */
+    void drawCircle(int r) override;
+
+    /**
+     * Funkcja wypisujaca tekst: Drawing a parallelogram from vectors (a1, a2), (b1, b2).
+     * @param a1 - wspolrzedna x pierwszego wierzcholka
+     * @param a2 - wspolrzedna y pierwszego wierzcholka
+     * @param b1 - wspolrzedna x drugiego wierzcholka
+     * @param b2 - wspolrzedna y drugiego wierzcholka
+     */
+    void drawParallelogram(int a1, int a2, int b1, int b2) override;
 };
 
