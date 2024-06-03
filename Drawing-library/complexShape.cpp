5eb9773cb47be0c90893f88c694c7d6f92066399
agentnyrro <qwerty2name@gmail.com>
2024-06-03 21:05:35 +0200

    add throw excpt statement to ComplexShape::add

--- Drawing-library/complexShape.cpp
+++ Drawing-library/complexShape.cpp
@@ -4,6 +4,9 @@
 ComplexShape::ComplexShape(Display* w) : Shape(w) {}
 
 bool ComplexShape::add(Shape* o) {
+    if (o == nullptr)
+        throw MyExceptions("Nieprawidlowy argument.\n");
+
     if (shapes.size() < MAX_SHAPES) {
         shapes.push_back(o);
         return true;
