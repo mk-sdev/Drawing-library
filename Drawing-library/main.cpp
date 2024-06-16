745844cab7772fb4eb8c3a501175c272a58732d2
agentnyrro <qwerty2name@gmail.com>
2024-06-16 22:19:54 +0200

    make TextDisplay::draw methods output text in Eng

--- Drawing-library/main.cpp
+++ Drawing-library/main.cpp
@@ -9,27 +9,47 @@
 #include "shape.h"
 using namespace std;
 int main() {
+	// tworzenie wyswietlaczy
 	Display* w1 = new GraphicalDisplay();
 	Display* w2 = new TextDisplay();
+
+	//tworzenie figur
 	Shape* o1 = new Triangle(w1, 3, 2, 2, -2);
 	Shape* o2 = new Parallelogram(w1, 3, 2, 2, -2);
 	Shape* o3 = new Circle(w1, 5);
 	ComplexShape* o4 = new ComplexShape(w1);
-	o4->add(o1);
-	o4->add(o2);
-	o4->add(o3);
-	o4->draw();
-	o1->draw();
-	o2->draw();
-	o3->draw();
-	o1->changeDisplay(w2);
-	o2->changeDisplay(w2);
-	o3->changeDisplay(w2);
-	o4->changeDisplay(w2);
-	o4->draw();
-	o1->draw();
-	o2->draw();
-	o3->draw();
+
+	try
+	{
+		// dodanie figur do zlozonej figury
+		o4->add(o1);
+		o4->add(o2);
+		o4->add(o3);
+		o4->draw();
+
+		// rysuj figury graficznie
+		o1->draw();
+		o2->draw();
+		o3->draw();
+
+		// zmien wyswietlacz
+		o1->changeDisplay(w2);
+		o2->changeDisplay(w2);
+		o3->changeDisplay(w2);
+		o4->changeDisplay(w2);
+
+		// rysuj tekstowo
+		o4->draw();
+		o1->draw();
+		o2->draw();
+		o3->draw();
+	}
+	catch (const exception& e)
+	{
+		cerr << e.what();
+	}
+	
+	// zwolnienie pamieci
 	delete o1;
 	delete o2;
 	delete o3;
